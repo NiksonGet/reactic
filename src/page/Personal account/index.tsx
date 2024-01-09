94c4f49479251d9e8859b2933323cca87ea5c941
NiksonGet <nik.ket.2016@mail.ru>
2024-01-09 21:02:27 +0300

    Module3

---
+++ src/page/Personal account/index.tsx
@@ -0,0 +1,11 @@
+const Account = () => {
+    return ( 
+        <>
+        <h1 className="heading">
+           Страница Аккаунта
+        </h1>
+        </>
+     );
+}
+ 
+export default Account;
